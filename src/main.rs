b7fcbfb7e740de2df15069458afe5bb8a63faa9b
jonny <johnnybarda423@gmail.com>
2024-04-16 12:08:32 +0300

    phase

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
 // Uncomment this block to pass the first stage
 
-use tokio::net::TcpListener;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tokio::net::{TcpListener, TcpStream};
+
+async fn handler(mut stream: TcpStream) -> anyhow::Result<()> {
+    let (mut reader, mut writer) = stream.split();
+    let mut reader = BufReader::new(reader);
+    let mut x = String::new();
+    let z = reader.read_line(&mut x).await?; // HTTP/1.1 200 OK\r\n
+    println!("{}", z);
+
+    x.clear();
+    let z = reader.read_line(&mut x).await?; // \r\n
+    println!("{}", z);
+
+    writer.write_all(
+        format!("HTTP/1.1 200 OK\r\n\r\n").as_bytes()
+    ).await?;
+
+    Ok(())
+}
+
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:4221").await?;
+    let addr = "127.0.0.1:4221";
+    let listener = TcpListener::bind(addr).await?;
+    println!("listening {addr}");
 
     loop {
-        let (_stream, _) = listener.accept().await?;
-        println!("accepted new connection");
+        let (stream, _) = listener.accept().await?;
+        if let Err(err) = handler(stream).await {
+            eprintln!("ERROR: connection ended with {err}")
+        }
     }
 }
